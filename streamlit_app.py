93753823971dbd349abd4fc2d6d27b542785d80d
Kelly Rao <112576923+madaowarrior@users.noreply.github.com>
2022-09-01 11:40:22 -0500

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('AR/VR Overview: Predictions and Projects Over the Next 5 Years')
 
-streamlit.header('Types of Compute')
-streamlit.text('External')
-streamlit.text('Internal')
-streamlit.text('Wireless/5g')
+streamlit.header('Compute')
+streamlit.text('🥭External🥭')
+streamlit.text('🥝Internal🥝')
+streamlit.text('🍇Wireless/5g🍇')
