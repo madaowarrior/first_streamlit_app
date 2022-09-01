6b6b03f474e7ff4f0a8e01868144f7eeda98dba8
Kelly Rao <112576923+madaowarrior@users.noreply.github.com>
2022-09-01 11:13:21 -0500

    Update streamlit_app.py
    Removed "Subtitle"

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit
 
 streamlit.title('AR/VR Overview: Predictions and Projects Over the Next 5 Years')
 
-streamlit.subtitle('Compute')
-
 streamlit.header('Types of Compute')
 streamlit.text('External')
 streamlit.text('Internal')
