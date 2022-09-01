c3bf2d8f6ec88415afcb583e8051380ff1da65e8
Kelly Rao <112576923+madaowarrior@users.noreply.github.com>
2022-09-01 11:12:30 -0500

    Update streamlit_app.py
    Formatting changes for functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title('AR/VR Overview: Predictions and Projects Over the Next 5 Years'
 streamlit.subtitle('Compute')
 
 streamlit.header('Types of Compute')
-streamlit.text(   'External')
-streamlit.text(   'Internal')
-streamlit.text(   'Wireless/5g')
+streamlit.text('External')
+streamlit.text('Internal')
+streamlit.text('Wireless/5g')
