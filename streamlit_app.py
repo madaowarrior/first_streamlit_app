6e873e59107b3d6da4af1d71eee3256300fb5336
Kelly Rao <112576923+madaowarrior@users.noreply.github.com>
2022-09-01 11:49:50 -0500

    Update streamlit_app.py
    Added fruit picker multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,10 @@ streamlit.header('World Building Assets')
 
 import pandas
 my_world_building_assets = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_world_building_assets = my_world_building_assets.set_index('Fruit')
 
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_world_building_assets)
