79aec9ba79d24aa746a3c132105790cd9fba8963
Kelly Rao <112576923+madaowarrior@users.noreply.github.com>
2022-09-01 11:11:55 -0500

    Update streamlit_app.py
    Changed function call from title to header
    made formatting changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit
 
-streamlit.title("AR/VR Overview: Predictions and Projects Over the Next 5 Years")
+streamlit.title('AR/VR Overview: Predictions and Projects Over the Next 5 Years')
 
-streamlit.subtitle("Compute")
+streamlit.subtitle('Compute')
 
-streamlit.text(
-               "Types of Compute"
-                    "External"
-                    "Internal"
-                    "Wireless/5g")
+streamlit.header('Types of Compute')
+streamlit.text(   'External')
+streamlit.text(   'Internal')
+streamlit.text(   'Wireless/5g')
