f0730611f81bc178ec3f4413cb8ba30ce71b97a6
Kelly Rao <112576923+madaowarrior@users.noreply.github.com>
2022-09-01 11:09:11 -0500

    Update streamlit_app.py
    Changed title
    Added subtitle
    Added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
-streamlit.title("My dog ate my presentation")
+streamlit.title("AR/VR Overview: Predictions and Projects Over the Next 5 Years")
+
+streamlit.subtitle("Compute")
+
+streamlit.text(
+               "Types of Compute"
+                    "External"
+                    "Internal"
+                    "Wireless/5g")
