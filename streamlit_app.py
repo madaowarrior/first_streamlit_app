868ea41654349fd949aba84537fb2143ccd6bd32
Kelly Rao <112576923+madaowarrior@users.noreply.github.com>
2022-09-01 11:45:38 -0500

    Update streamlit_app.py
    imported fruits data to world building dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,8 @@ streamlit.text('🥝Internal🥝')
 streamlit.text('🍇Wireless/5g🍇')
 
 streamlit.header('World Building')
+
+import pandas
+my_world_building_assets = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+
+streamlit.dataframe(my_world_building_assets)
