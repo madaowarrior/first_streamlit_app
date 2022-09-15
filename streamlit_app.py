2de62e2c61b120663740f4fd1c69dd36b345b55d
Kelly Rao <112576923+madaowarrior@users.noreply.github.com>
2022-09-15 15:00:17 -0500

    Update streamlit_app.py
    removed jason print, created string separation using dbl quot url for url base

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ streamlit.dataframe(assets_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+
 
 # take jason version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
